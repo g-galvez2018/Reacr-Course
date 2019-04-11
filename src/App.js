000005bc012532d3b0dd303409ec155f5f884570
Gustavo Galvez <gustavo.galvez09@gmail.com>
2019-04-10 23:53:02 -0400

    Up to third lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max' , age: 28},
+      {name: 'Manu' , age: 29},
+      {name: 'Stephanie' , age: 26}
+    ]
+  }
+
+  swicthNameHandler = (newName) =>{
+    //console.log("this was clicked");
+    this.setState({
+      persons: [
+        {name: 'Maximilian' , age: 28},
+        {name: newName , age: 29},
+        {name: 'Stephanie' , age: 27}
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Max' , age: 28},
+        {name: event.target.value , age: 29},
+        {name: 'Steph' , age: 27}
+      ]
+    })
+  }
+
+ 
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, I am a React APP</h1>
+        <p>This is really working!</p>
+        <button onClick={() => this.swicthNameHandler ('Gus')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          changed={this.nameChangedHandler} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.swicthNameHandler.bind(this,'Gustavo')}
+          changed={this.nameChangedHandler}>My hobbies: Racing </Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}
+          changed={this.nameChangedHandler} />
       </div>
     );
   }
